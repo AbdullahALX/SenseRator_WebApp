4774849e95b6e2447f8b43584823df344e32062a
Abdullah AL Hinaey <101839293+AbdullahALX@users.noreply.github.com>
2024-08-21 18:19:07 -0400

    landing page is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import { Button } from '@nextui-org/react';
 
 import axios from 'axios';
+import Hero from './pages/hero/Hero';
+import NavBar from './pages/navbar/Navbar';
 
 function App() {
   const url = 'http://localhost:3002';
@@ -24,12 +27,17 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen ">
-      <p className="  text-black text-center text-2xl">
-        Senserator 2.0 web app
-      </p>
-      <p className="text-black text-center text-2xl">{data}</p>
-    </div>
+    <>
+      {/* <NavBar /> */}
+      <Hero />
+    </>
+    // <div className="flex flex-col justify-center items-center h-screen ">
+    //   <p className="  text-black text-center text-2xl">
+    //     Senserator 2.0 web app
+    //   </p>
+    //   <p className="text-black text-center text-2xl">{data}</p>
+    //   <Button color="primary">Button</Button>
+    // </div>
   );
 }
 
---
+++ frontend/src/pages/hero/Hero.jsx
@@ -0,0 +1,131 @@
+import { Chip } from '@nextui-org/chip';
+import { Button } from '@nextui-org/button';
+import { motion } from 'framer-motion';
+
+import { Input } from '@nextui-org/react';
+import { Image } from '@nextui-org/react';
+
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from '@nextui-org/navbar';
+import { Link } from '@nextui-org/link';
+import ThemeSwitcher from '../../components/ThemeSwitcher';
+
+import logo from '../../assets/logo3.png';
+
+export default function HeroWithNavBar() {
+  const menuItems = ['Map', 'Docs', 'Blog'];
+
+  return (
+    <div className=" h-screen overflow-hidden">
+      <Navbar isBlurred maxWidth="xl">
+        <NavbarContent className="sm:hidden" justify="start">
+          <NavbarMenuToggle />
+        </NavbarContent>
+        <NavbarContent className="pr-3" justify="center">
+          <NavbarBrand>
+            <Image width={150} src={logo} />
+          </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent className="hidden sm:flex gap-10 ">
+          <NavbarBrand>
+            <Image width={250} src={logo} />
+          </NavbarBrand>
+          <NavbarItem>
+            <Button as={Link} variant="light">
+              Map
+            </Button>
+          </NavbarItem>
+          <NavbarItem>
+            <Button as={Link} variant="light">
+              Docs
+            </Button>
+          </NavbarItem>
+          <NavbarItem>
+            <Button as={Link} variant="light">
+              Blog
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem>
+            <ThemeSwitcher />
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarMenu className="bg-black justify-center items-center gap-10">
+          {menuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link className="w-full " href="#" size="lg">
+                {item}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
+
+      <div className="relative justify-center items-center h-full flex overflow-y-hidden">
+        <div className="absolute left-0 right-0 bottom-0 top-0 z-0">
+          <Image
+            className="w-full h-full"
+            src="https://nextui.org/gradients/docs-left.png"
+            alt="gradient"
+          />
+        </div>
+        <section className="max-w-screen-xl mx-auto px-4 py-28 gap-12 md:px-8 flex flex-col justify-center items-center">
+          <motion.div
+            initial={{ y: 5, opacity: 0 }}
+            animate={{
+              y: 0,
+              opacity: 1,
+            }}
+            transition={{ duration: 0.5 }}
+            className="flex flex-col justify-center items-center space-y-5 max-w-4xl mx-auto text-center"
+          >
+            <Chip variant="shadow" color="default">
+              Experience AI-Driven Urban Planning!
+            </Chip>
+            <h1 className="text-4xl font-light tracking-tighter mx-auto md:text-6xl bg-gradient-to-b from-foreground to-foreground/70 text-transparent bg-clip-text text-pretty">
+              Transform Cities with{' '}
+              <span className="bg-gradient-to-t from-light to-foreground text-transparent bg-clip-text border-none">
+                SenseRator 2.0
+              </span>
+            </h1>
+            <p className="max-w-2xl mx-auto text-foreground/80 text-balance">
+              Semi-Real-Time Mapping and AI Analysis for Smarter, Safer, and
+              More Livable Urban Spaces.
+            </p>
+            <Input
+              type="email"
+              label="Email"
+              description="Stay updated with the latest in AI-driven urban planning technology."
+              className="max-w-lg"
+            />
+            <div className="items-center justify-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
+              <motion.div whileHover={{ scale: 1.05 }}>
+                <Button color="primary" variant="solid">
+                  Get Started
+                </Button>
+              </motion.div>
+            </div>
+          </motion.div>
+        </section>
+        <motion.div
+          initial={{ y: 5, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.7, delay: 0.5 }}
+          className="w-full h-full absolute -top-32 flex justify-end items-center -z-10"
+        >
+          <div className="w-3/4 flex justify-center items-center">
+            <div className="w-12 h-[600px] bg-light blur-[100px] rounded-3xl max-sm:rotate-[15deg] sm:rotate-[35deg]"></div>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+}
