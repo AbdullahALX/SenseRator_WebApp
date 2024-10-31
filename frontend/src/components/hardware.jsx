f361ea88ab107e3b52c96c887a0cac61fc9791bc
Kristian Michel <kristianjeanmichel@gmail.com>
2024-10-31 13:19:14 -0400

    modified hardware and psfi pages

--- frontend/src/components/hardware.jsx
+++ frontend/src/components/hardware.jsx
@@ -7,48 +7,66 @@ const hardware = () => {
         <div className="mb-4 p-4 border border-foreground-300 rounded-lg bg-foreground-50 ">
           <h3 className="text-xl font-bold mb-2">Jetson Nano</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-            voluptatibus ullam ipsum, atque, consequatur eligendi laudantium
-            voluptas pariatur quo placeat beatae sit ducimus unde, eius
-            reprehenderit ut dolor reiciendis accusantium!
+          The Jetson Nano is a compact yet powerful computing platform developed by NVIDIA, 
+          tailored for Machine Learning applications and edge computing. Serving as the brain of SenseRator 2.0, 
+          it allows our team to run our complex programs required for the real-time data analysis we need to accomplish. 
+          It is equipped with a powerful CPU and GPU necessary to run advanced object detection and tracking algorithms efficiently on the move.
+          <br/><br/>
+          In the context of the SenseRator 2.0, the Jetson Nano processes live video feeds from the RunCam2, 
+          identifying and quantifying key urban features: sidewalks, crosswalks, benches, street lights, stop signs, 
+          and trees. Its ability to handle multiple neural networks in parallel ensures that our data is processed in real time which allows us to 
+          calculate the PFS Score on the fly. The device’s energy efficiency and compact form make it ideal for mobile deployment, 
+          allowing us to seamlessly integrate our bicycle-mounted setup without excessive weight or bulk.
           </p>
         </div>
 
         <div className="mb-4 p-4 border border-foreground-300 rounded-lg bg-foreground-50">
           <h3 className="text-xl font-bold mb-2">Runcam 2</h3>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
-            id, in quos possimus nam iste exercitationem doloribus sed? Soluta
-            beatae saepe reprehenderit reiciendis. Mollitia quas facilis
-            deleniti aut unde pariatur.
+          The RunCam 2 is a high-definition, action camera renowned for it's lightweight, compact design, and great image quality. 
+          It serves as the visual sensor for the SenseRator 2.0, capturing crisp and detailed footage of the urban environment as the system moves through the cityscape.
+          With it's recording capabilities in 1080p at 60fps, the RunCam 2 provided us the necessary clarity and frame rate needed for accurate object detection and tracking.
+          <br/><br/>
+          The wide-angle lens captures a broad field of view, ensuring that all relevant features within the environment being analyzed are included in the video feed. 
+          The camera’s compact size allows it to be integrated easily to our custom 3D-printed mount, designed to fit to the DJI OM5 gimbal, also keeping our detection 
+          system minimal in size.
           </p>
         </div>
 
         <div className="mb-4 p-4 border border-foreground-300 rounded-lg bg-foreground-50">
           <h3 className="text-xl font-bold mb-2">Camera stabilizer</h3>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
-            id, in quos possimus nam iste exercitationem doloribus sed? Soluta
-            beatae saepe reprehenderit reiciendis. Mollitia quas facilis
-            deleniti aut unde pariatur.
+            The Camera Stabilizer is a critical component to our video collection, 
+            allowing the RunCam 2 to capture smooth and steady video feed free from distortions caused by uneven and aggressive movements. 
+            While testing initial versions of the SenseRator 2.0, 
+            we encountered bumpy and rough terrains that greatly affected the object detection model to efficiently label objects. 
+            The SenseRator 2.0’s mounting system combines a DJI OM5 gimbal for X/Y - axis stabilization with our custom-designed Z-axis spring gimbal to address 
+            vertical movements when the bike encounters uneven terrain.
+            <br/><br/>
+            The DJI gimbal is a professional-grade stabilizer that keeps the camera steady against horizontal and rotational motions, 
+            providing a stable platform to mount the lightweight RunCam 2. 
+            To complement this, a custom Z-axis spring gimbal was designed using small joints and springs inside a 
+            3D-printed model to absorb and counteract vertical jolts and vibrations that occur while cycling. 
+            The multi-axis stabilization system is essential for maintaining high-quality video input, 
+            directly impacting the effectiveness and accuracy of the detection algorithms running on the Jetson Nano.
+
+            The camera-stabilization system is ultimately attached to the bicycle using a NOGA Israeli Arm. 
+            The NOGA arm locks via pressurization in it's joints to securely support the weight and bulk of the system out in front of the bicycle and Jetson Nano.
           </p>
         </div>
         <div className="mb-4 p-4 border border-foreground-300 rounded-lg bg-foreground-50">
           <h3 className="text-xl font-bold mb-2">Vehicle mount</h3>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
-            id, in quos possimus nam iste exercitationem doloribus sed? Soluta
-            beatae saepe reprehenderit reiciendis. Mollitia quas facilis
-            deleniti aut unde pariatur.
-          </p>
-        </div>
-        <div className="mb-4 p-4 border border-foreground-300 rounded-lg bg-foreground-50">
-          <h3 className="text-xl font-bold mb-2">Small monitor</h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
-            id, in quos possimus nam iste exercitationem doloribus sed? Soluta
-            beatae saepe reprehenderit reiciendis. Mollitia quas facilis
-            deleniti aut unde pariatur.
+            The Vehicle Mount is custom designed to securely attach all the components of the SenseRator 2.0 to a bicycle. 
+            Crafted with 3D printing, the mount is tailored to fit the unique dimensions of the Jetson Nano and its 
+            accompanying LCD 5” touch screen and to lock them to the handlebars of the bicycle.
+            <br/><br/>
+            Constructed from lightweight plastic and small hardware, the vehicle mount ensures that the equipment is safely secured to the bicycle and 
+            does not impede on the ability of the cyclist to navigate through the environment. 
+            The mount facilitates quick assembly and disassembly, allowing for efficient deployment and maintenance of the system.
+            <br/><br/>
+            Along with the touch screen monitor, this allows our team to view our work in real time and monitor feedback as well as interacting with our system. 
+            Through this system, we can make on-the-fly adjustments to parameters in our code, processing, and model.
           </p>
         </div>
       </div>
--- frontend/src/components/psfi.jsx
+++ frontend/src/components/psfi.jsx
@@ -1,44 +1,116 @@
 import React from 'react';
 import PSFICalculation from './psfi-calculation';
 import DataCollection from './dataCollection';
+import {  Table,  TableHeader,  TableBody,  TableColumn,  TableRow,  TableCell} from "@nextui-org/table";
 
 const PSFI = () => {
+  
+  const rows = [
+    {
+      key: "1",
+      feature: "Trees",
+      pointsPerObject: 1,
+      maximumScore: 10,
+      weight: 10,
+    },
+    {
+      key: "2",
+      feature: "Crosswalks",
+      pointsPerObject: 2,
+      maximumScore: 20,
+      weight: 20,
+    },
+    {
+      key: "3",
+      feature: "Street Lights",
+      pointsPerObject: 2,
+      maximumScore: 20,
+      weight: 20,
+    },
+    {
+      key: "4",
+      feature: "Sidewalks",
+      pointsPerObject: 5,
+      maximumScore: 25,
+      weight: 25,
+    },
+    {
+      key: "5",
+      feature: "Benches",
+      pointsPerObject: 1,
+      maximumScore: 10,
+      weight: 10,
+    },
+    {
+      key: "6",
+      feature: "Stop Signs",
+      pointsPerObject: 1.5,
+      maximumScore: 15,
+      weight: 15,
+    },
+  ];
+  
+  const columns = [
+    {
+      key: "feature",
+      label: "Features",
+    },
+    {
+      key: "pointsPerObject",
+      label: "Points Per Object",
+    },
+    {
+      key: "maximumScore",
+      label: "Maximum Score",
+    },
+    {
+      key: "weight",
+      label: "Weight",
+    },
+  ];
+
   return (
     <div className="p-4 overflow-scroll">
       <div className="mb-4 p-4 border border-foreground-300 rounded-lg bg-foreground-50">
         <h3 className="text-xl font-bold mb-2">What is it</h3>
         <p>
-          The Pedestrian Flow and Safety (PFS) Score is a rating method that
-          measures the efficiency and safety of pedestrian navigation in urban
-          spaces. It considers elements such as: sidewalks, crosswalks, trees,
-          traffic lights, benches, and stop signs. However, it should be noted
-          that safe pedestrian structures are not limited to the chosen
-          architecture this project utilizes.
-          <br />
-          Scoring begins by using our object detection and tracking algorithm to
-          capture our desired structures for scoring. The total objects found
-          are then weighted, and compiled to create our scoring, which is then
-          displayed on our application
+          The Pedestrian Flow and Safety (PFS) Score is a rating method that measures the efficiency and safety of pedestrian navigation in urban spaces. 
+          It considers elements such as: sidewalks, crosswalks, trees, street lights, benches, and stop signs. Scoring begins by 
+          using our object detection and tracking algorithm to capture our desired features for scoring. 
+          The total objects found are then weighted, and compiled to create our scoring metric, which is then displayed on our application.
         </p>
       </div>
       <div className="mb-4 p-4 border border-foreground-300 rounded-lg bg-foreground-50">
         <h3 className="text-xl font-bold mb-2">Features</h3>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-          inventore ullam modi omnis officia. Provident, corporis quo, iusto
-          ducimus, at magni voluptatum debitis sequi libero laborum quidem.
-          Tenetur, repudiandae in.
+          <h4 className="text-md font-bold" >Sidewalks: </h4> Sidewalks are measured to assess the availability of safe, dedicated walking paths, impacting overall pedestrian accessibility. 
+          <br/><br/>
+          <h4 className="text-md font-bold" >Crosswalks: </h4> Crosswalks are included to evaluate safe street-crossing options, directly impacting pedestrian safety and transportation effectiveness.
+          <br/><br/>
+          <h4 className="text-md font-bold" >Trees: </h4> Trees are measured to account for environmental comfort, as they provide shade, improve air quality, and aesthetics of an area.
+          <br/><br/>
+          <h4 className="text-md font-bold" >Street Lights: </h4> Street lights are assessed to determine nighttime visibility, impacting both pedestrian safety and overall accessibility.
+          <br/><br/>
+          <h4 className="text-md font-bold" >Benches: </h4> Benches are considered to evaluate resting opportunities, supporting accessibility and comfort for longer walks.
+          <br/><br/>
+          <h4 className="text-md font-bold" >Stop Signs: </h4> Stop signs are included to gauge intersection safety, as they help manage vehicle flow and make pedestrian crossings safer.
         </p>
       </div>
 
       <div className="mb-4 p-4 border border-foreground-300 rounded-lg bg-foreground-50">
         <h3 className="text-xl font-bold mb-2">Calculations</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-          inventore ullam modi omnis officia. Provident, corporis quo, iusto
-          ducimus, at magni voluptatum debitis sequi libero laborum quidem.
-          Tenetur, repudiandae in.
-        </p>
+          <Table aria-label="Example table with dynamic content">
+            <TableHeader columns={columns}>
+              {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+            </TableHeader>
+            <TableBody items={rows}>
+              {(item) => (
+                <TableRow key={item.key}>
+                  {(columnKey) => <TableCell>{item[columnKey]}</TableCell>}
+                </TableRow>
+              )}
+            </TableBody>
+        </Table>
       </div>
     </div>
   );
